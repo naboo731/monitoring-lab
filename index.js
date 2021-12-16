9da37f586e4966e9d03cc744797be45137d3738d
Nichole Boothman <nbooth731@gmail.com>
2021-12-16 13:57:40 -0500

    rollbar added

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'c82ddf2d8c4f46d99d3bab6013d0e2ae',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+const port = process.env.PORT || 4545
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html loaded successfully')
 })
 
-const port = process.env.PORT || 4545
+
+
+
+
+
+
 
 
 app.listen(port, () => {
